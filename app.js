4b735c065eb6f648211468a737fe47ce2a477b0d
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-08-25 16:38:37 -0700

    Finished Animal-Farm

--- app.js
+++ app.js
@@ -1,9 +1,43 @@
 // import functions
+const pigImage = document.getElementById('pig-image')
+const pigAudio = document.getElementById('horse-sound')
+const pigSpan = document.getElementById('pig-span')
+
+const elImage = document.getElementById('el-image')
+const elAudio = document.getElementById('cat-sound')
+const elSpan = document.getElementById('el-span')
+
+const birdImage = document.getElementById('bird-image')
+const birdAudio = document.getElementById('dog-sound')
+const birdSpan = document.getElementById('bird-span')
+
+let pigClicks = 0;
+let elClicks = 0;
+let birdClicks = 0;
 
 // reference needed DOM elements
 
 
 // set event listeners 
+pigImage.addEventListener('click', () => {
+    pigAudio.play();
+  pigClicks ++
+  pigSpan.textContent = 'You have clicked the pig ' + pigClicks + ' times!';
+
+});
+
+elImage.addEventListener('click', () => {
+    elAudio.play();
+    elClicks ++
+    elSpan.textContent = 'You have clicked the elephant ' + elClicks + ' times!';
+
+});
+
+birdImage.addEventListener('click', () => {
+    birdAudio.play();
+    birdClicks ++
+    birdSpan.textContent = 'You have clicked the bird ' + birdClicks + ' times!'
+});
   // get user input(s)
   // do any needed work with the value(s)
   // update DOM to reflect new value(s)
