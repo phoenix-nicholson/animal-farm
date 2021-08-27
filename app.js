bbda5894497b1ef4ca4972fa367e82e9cfa13639
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2021-08-26 17:02:51 -0700

    Completed test

--- app.js
+++ app.js
@@ -1,15 +1,15 @@
 // import functions
-const pigImage = document.getElementById('pig-image')
-const pigAudio = document.getElementById('horse-sound')
-const pigSpan = document.getElementById('pig-span')
+const pigImage = document.getElementById('pig-image');
+const pigAudio = document.getElementById('horse-sound');
+const pigSpan = document.getElementById('pig-span');
 
-const elImage = document.getElementById('el-image')
-const elAudio = document.getElementById('cat-sound')
-const elSpan = document.getElementById('el-span')
+const elImage = document.getElementById('el-image');
+const elAudio = document.getElementById('cat-sound');
+const elSpan = document.getElementById('el-span');
 
-const birdImage = document.getElementById('bird-image')
-const birdAudio = document.getElementById('dog-sound')
-const birdSpan = document.getElementById('bird-span')
+const birdImage = document.getElementById('bird-image');
+const birdAudio = document.getElementById('dog-sound');
+const birdSpan = document.getElementById('bird-span');
 
 let pigClicks = 0;
 let elClicks = 0;
@@ -21,22 +21,22 @@ let birdClicks = 0;
 // set event listeners 
 pigImage.addEventListener('click', () => {
     pigAudio.play();
-  pigClicks ++
-  pigSpan.textContent = 'You have clicked the pig ' + pigClicks + ' times!';
+    pigClicks ++;
+    pigSpan.textContent = 'You have clicked the pig ' + pigClicks + ' times!';
 
 });
 
 elImage.addEventListener('click', () => {
     elAudio.play();
-    elClicks ++
+    elClicks ++;
     elSpan.textContent = 'You have clicked the elephant ' + elClicks + ' times!';
 
 });
 
 birdImage.addEventListener('click', () => {
     birdAudio.play();
-    birdClicks ++
-    birdSpan.textContent = 'You have clicked the bird ' + birdClicks + ' times!'
+    birdClicks ++;
+    birdSpan.textContent = 'You have clicked the bird ' + birdClicks + ' times!';
 });
   // get user input(s)
   // do any needed work with the value(s)
